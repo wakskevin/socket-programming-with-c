7efffb339d1f0797445973c8ba6927e3b06786c6
K-Waks <wakskevin@outlook.com>
2023-04-11 12:42:50 +0300

    added user reg and name input functionality

---
+++ main.c
@@ -0,0 +1,61 @@
+// To write a program that requests a user to enter their RegNo, name and save them to a file.
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+    char regno[20];
+    char fname[15];
+    char lname[15];
+    char confirm[5];
+    int trial = 1;
+
+    while (trial <= 3)
+    {
+        puts("*************************************");
+        printf("Your registration number? ");
+        scanf("%s", regno);
+        printf("Your first name? ");
+        scanf("%s", fname);
+        printf("Your last name? ");
+        scanf("%s", lname);
+
+        putchar('\n');
+        printf("You are %s %s of registration %s.\n", fname, lname, regno);
+        printf("Is this correct? (type 'yes' or 'no'): ");
+        scanf("%s", confirm);
+
+        if (strcmp(confirm, "yes") == 0)
+        {
+            puts("Record added to file ✅");
+            break;
+        }
+        else if (strcmp(confirm, "no") == 0)
+        {
+            printf("trials done: %d of 3\n", trial);
+            if (trial == 3)
+            {
+                puts("Trial limit reached! Exiting program...");
+            }
+            putchar('\n');
+            trial++;
+        }
+        else
+        {
+            puts("Invalid input (Allowed values are 'yes' or 'no')");
+            printf("trials done: %d of 3\n", trial);
+            if (trial == 3)
+            {
+                puts("Trial limit reached! Exiting program...");
+            }
+            putchar('\n');
+            trial++;
+        }
+    }
+
+    putchar('\n');
+
+    return (0);
+}
