2b08ce4c1c8c47e0736861178525733a4957dd99
K-Waks <wakskevin@outlook.com>
2023-04-11 12:59:27 +0300

    small changes

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 // To write a program that requests a user to enter their RegNo, name and save them to a file.
 
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
@@ -9,7 +8,7 @@ int main()
     char regno[20];
     char fname[15];
     char lname[15];
-    char confirm[5];
+    char confirm;
     int trial = 1;
 
     while (trial <= 3)
@@ -24,15 +23,15 @@ int main()
 
         putchar('\n');
         printf("You are %s %s of registration %s.\n", fname, lname, regno);
-        printf("Is this correct? (type 'yes' or 'no'): ");
-        scanf("%s", confirm);
+        printf("Is this correct? (type 'y' or 'n'): ");
+        scanf(" %c", &confirm);   // don't remove the whitespace before %c
 
-        if (strcmp(confirm, "yes") == 0)
+        if (confirm == 'y')
         {
             puts("Record added to file ✅");
             break;
         }
-        else if (strcmp(confirm, "no") == 0)
+        else if (confirm == 'n')
         {
             printf("trials done: %d of 3\n", trial);
             if (trial == 3)
