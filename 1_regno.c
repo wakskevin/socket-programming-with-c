6a531a786a26dcb5746ccbc46f5ec79ebc49e753
K-Waks <wakskevin@outlook.com>
2023-04-12 09:02:31 +0300

    Split program into 3 - regno, name, regno_and_name

--- 1_regno.c
+++ 1_regno.c
@@ -1,14 +1,12 @@
 // To write a program that requests a user to enter their RegNo, name and save them to a file.
 
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-
     char regno[20];
-    char fname[15];
-    char lname[15];
-    char confirm;
+    char confirm[5];
     int trial = 1;
 
     while (trial <= 3)
@@ -16,22 +14,18 @@ int main()
         puts("*************************************");
         printf("Your registration number? ");
         scanf("%s", regno);
-        printf("Your first name? ");
-        scanf("%s", fname);
-        printf("Your last name? ");
-        scanf("%s", lname);
 
         putchar('\n');
-        printf("You are %s %s of registration %s.\n", fname, lname, regno);
-        printf("Is this correct? (type 'y' or 'n'): ");
-        scanf(" %c", &confirm);   // don't remove the whitespace before %c
+        printf("Your registration number is %s\n", regno);
+        printf("Is this correct? (type 'yes' or 'no'): ");
+        scanf("%s", confirm);
 
-        if (confirm == 'y')
+        if (strcmp(confirm, "yes") == 0)
         {
             puts("Record added to file ✅");
             break;
         }
-        else if (confirm == 'n')
+        else if (strcmp(confirm, "no") == 0)
         {
             printf("trials done: %d of 3\n", trial);
             if (trial == 3)
